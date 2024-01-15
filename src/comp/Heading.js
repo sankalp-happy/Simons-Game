33e215d93e7296431c8cd663cb5e5829cf199330
Sankalp Shankar <sankalps.23.beai@acharya.ac.in>
2024-01-15 22:00:38 +0530

    Added responsive design

--- src/comp/Heading.js
+++
@@ -1,8 +0,0 @@
-var Heading = props => { 
-    return(
-        <h1 id = "level-title" onClick={props.click}>
-            {(props.level<0)?"Game Over! Click again to restart.":props.level?"Level "+ props.level : "Click me to start!"}
-        </h1>
-    );
-}
-export default Heading;
